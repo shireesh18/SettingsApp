5025732f083116717de34a0be0b440143aa6e204
Shireesh Marla <shireesh.marla@swipiicard.com>
2021-11-30 10:33:09 +0000

    Initial Commit

---
+++ SettingsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingsApp
+//
+//  Created by Shireesh Marla on 30/11/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
